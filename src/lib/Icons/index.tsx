b9be6ca5191142c5a73776eb8ec2e089f0e8c0fb
lallaheeee <ooop0422@gmail.com>
2020-06-17 15:56:01 +0900

    chore(npm): svg 아이콘에 사용할 react-icons 모듈 설치
                쇼핑 카트 및 < > 화살표 아이콘 사용 예정

---
+++ src/lib/Icons/index.tsx
@@ -0,0 +1,9 @@
+export {
+	AiFillShopping as FillShopping,
+	AiOutlineShopping as OutlineShopping,
+} from "react-icons/ai";
+
+export {
+	RiArrowLeftSLine as LeftArrow,
+	RiArrowRightSLine as RightArrow,
+} from "react-icons/ri";
