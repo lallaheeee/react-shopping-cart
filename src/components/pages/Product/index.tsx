abc8e85661821f3d73f91200638a475ddee98bf8
lallaheeee <ooop0422@gmail.com>
2020-06-18 19:36:50 +0900

    feat(page): product page 작성
    상품 리스트를 노출하는 product page 작성
    추후 장바구니 상태관리 추가 후
    carts, onClickShoppingCart 추가 예정

--- src/components/pages/Product/index.tsx
+++ src/components/pages/Product/index.tsx
@@ -1,7 +1,22 @@
 import React from "react";
+import ProductList from "~/components/UI/blocks/ProductList";
+import Pagination from "~/components/UI/atoms/Pagination";
+import useProductList from "./useProductList";
+import BaseTemplate from "~/components/pages/BaseTemplate";
 
 const ProductPage = () => {
-	return <div> welcome, productPage! </div>;
+	const { page, products, totalPage, setPage } = useProductList();
+
+	return (
+		<BaseTemplate>
+			<ProductList
+				products={products}
+				carts={[]}
+				onClickShoppingCart={() => {}}
+			/>
+			<Pagination page={page} totalPage={totalPage} onChange={setPage} />
+		</BaseTemplate>
+	);
 };
 
 export default ProductPage;
