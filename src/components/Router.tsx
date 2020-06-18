0b028c3da19ee0bb06818fa5725cf336602b4c97
lallaheeee <ooop0422@gmail.com>
2020-06-18 23:27:16 +0900

    chore: 장바구니 컨텍스트 프로바이더 적용
    product, cart 페이지 둘다 사용하기 때문에
    상위에 적용

--- src/components/Router.tsx
+++ src/components/Router.tsx
@@ -1,12 +1,15 @@
 import React from "react";
 import { Switch, BrowserRouter as Router, Route } from "react-router-dom";
 import { Product, ShoppingCart } from "~/components/pages";
+import { ShoppingCartProvider } from "~/contexts/ShoppingCarts";
 
 export default () => (
 	<Router>
 		<Switch>
-			<Route path="/products" component={Product} />
-			<Route path="/carts" component={ShoppingCart} />
+			<ShoppingCartProvider>
+				<Route path="/products" component={Product} />
+				<Route path="/carts" component={ShoppingCart} />
+			</ShoppingCartProvider>
 		</Switch>
 	</Router>
 );
