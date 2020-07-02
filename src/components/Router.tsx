c790635131e9da06c74feed3bcfdddcbed6108fb
lallaheeee <ooop0422@gmail.com>
2020-07-02 19:53:58 +0900

    chore(route): / 로 접속시 /products 로 리다이렉트
    / 로 접속시 빈 화면이기에, products로 리다이렉트

--- src/components/Router.tsx
+++ src/components/Router.tsx
@@ -1,12 +1,13 @@
 import React from "react";
 import { Switch, BrowserRouter as Router, Route } from "react-router-dom";
-import { Product, ShoppingCart } from "~/components/pages";
+import { Home, Product, ShoppingCart } from "~/components/pages";
 import { ShoppingCartProvider } from "~/contexts/ShoppingCarts";
 
 export default () => (
 	<Router>
 		<Switch>
 			<ShoppingCartProvider>
+				<Route path="/" component={Home} />
 				<Route path="/products" component={Product} />
 				<Route path="/carts" component={ShoppingCart} />
 			</ShoppingCartProvider>
--- src/components/pages/index.ts
+++ src/components/pages/index.ts
@@ -1,2 +1,3 @@
+export { default as Home } from "./Home";
 export { default as Product } from "./Product";
 export { default as ShoppingCart } from "./ShoppingCart";
